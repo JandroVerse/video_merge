ce7bfc6ab2ab61519cfe1b6e6fa88d9bb8c9422f
JandroVerse <aalfo021@gmail.com>
2024-11-25 18:25:15 -0500

    Add files via upload
    fixing Port and Host for Render

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def download_file(filename):
     return "File not found!", 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
